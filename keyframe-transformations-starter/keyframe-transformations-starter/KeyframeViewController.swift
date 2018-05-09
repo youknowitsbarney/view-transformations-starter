5a81ff71401a11984306ee38158cee7b76fa3965
Barney <barneym29@gmail.com>
2018-05-09 12:07:22 +0300

    Setup II

---
+++ keyframe-transformations-starter/keyframe-transformations-starter/KeyframeViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+class KeyframeViewController: UIViewController {
+
+    @IBOutlet weak var blueView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func animate(_ sender: UIButton) {
+    }
+}
